813f8a93a170b28115f36826804fb1ad87fe2183
Joao Oliveira <j.andre.oliveira@gmail.com>
2017-01-15 13:00:03 +0100

    refactored retrieving colors

--- app/src/main/java/com/longerweekends/longerweekends/MainActivity.java
+++ app/src/main/java/com/longerweekends/longerweekends/MainActivity.java
@@ -12,7 +12,9 @@ import com.roomorama.caldroid.CaldroidListener;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.longerweekends.longerweekends.SelectionMode.AL;
 import static com.longerweekends.longerweekends.SelectionMode.C;
@@ -22,16 +24,20 @@ import static com.longerweekends.longerweekends.SelectionMode.SELECT;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final Map<SelectionMode, ColorDrawable> COLORS = new HashMap<>(4);
+
+    static {
+        COLORS.put(AL, new ColorDrawable(Color.GREEN));
+        COLORS.put(PL, new ColorDrawable(Color.YELLOW));
+        COLORS.put(C, new ColorDrawable(Color.MAGENTA));
+        COLORS.put(SELECT, new ColorDrawable(Color.WHITE));
+    }
+
     private int entitledLeaves;
     private List<Date> approvedLeaves = new ArrayList<>();
     private List<Date> plannedLeaves = new ArrayList<>();
     private SelectionMode selectionMode = SELECT;
     private LongWeekendsCalendar longWeekendsCalendar;
-
-    private static final ColorDrawable GREEN = new ColorDrawable(Color.GREEN);
-    private static final ColorDrawable YELLOW = new ColorDrawable(Color.YELLOW);
-    private static final ColorDrawable MAGENTA = new ColorDrawable(Color.MAGENTA);
-    private static final ColorDrawable DEFAULT_COLOR = new ColorDrawable(Color.WHITE);
 
     private EditText entitledLeavesTxt;
     private EditText balanceTxt;
@@ -44,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initializeHeader();
         buildCalendar();
-
     }
 
     private void initializeHeader() {
@@ -65,25 +70,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addListener() {
-        //TODO refactor to a custom listener that accepts a drawer.
-        //create different drawers for different colors and substitute the ifs.
         longWeekendsCalendar.setCaldroidListener(new CaldroidListener() {
 
             @Override
             public void onSelectDate(Date date, View view) {
                 if (selectionMode != SELECT) {
                     removeSelection(date);
-                    ColorDrawable color = DEFAULT_COLOR;
+                    ColorDrawable color = COLORS.get(selectionMode);
                     if (selectionMode == AL) {
-                        color = GREEN;
                         approvedLeaves.add(date);
                     } else if (selectionMode == PL) {
-                        color = YELLOW;
                         plannedLeaves.add(date);
-                    } else if (selectionMode == C) {
-                        color = MAGENTA;
                     } else if (selectionMode == REMOVE) {
-                        color = DEFAULT_COLOR;
                         removeSelection(date);
                     }
                     longWeekendsCalendar.setBackgroundDrawableForDate(color, date);
@@ -114,7 +112,6 @@ public class MainActivity extends AppCompatActivity {
 
     //When user clicks on selection mode
     public void updateListener(View view) {
-        //TODO Move to factory, avoid creating new instances
         if (R.id.approvedLeave == view.getId()) {
             selectionMode = AL;
         } else if (R.id.plannedLeave == view.getId()) {
